a899a806730f73b4f750d04d1152497b85ef57b0
fontilan <68748054+fontilan@users.noreply.github.com>
2022-04-11 19:59:42 +0200

    destructure props

--- src/Songs.js
+++ src/Songs.js
@@ -1,9 +1,12 @@
-function Songs(props) {
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+function Songs({ author, title }) {
   return (
     <div className="song">
-      <p className="song--author">{props.author}</p>
+      <p className="song--author">{author}</p>
       <p className="song--separator">&nbsp;—&nbsp;</p>
-      <p className="song--title">{props.title}</p>
+      <p className="song--title">{title}</p>
     </div>
   );
 }
