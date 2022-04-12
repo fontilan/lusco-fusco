89da414a16234e3ace3f401bffb2f277c336de3c
fontilan <68748054+fontilan@users.noreply.github.com>
2022-04-12 17:28:20 +0200

    change ps to divs

--- src/Songs.js
+++ src/Songs.js
@@ -4,9 +4,9 @@ import React from 'react';
 function Songs({ author, title }) {
   return (
     <div className="song">
-      <p className="song--author">{author}</p>
-      <p className="song--separator">&nbsp;—&nbsp;</p>
-      <p className="song--title">{title}</p>
+      <div className="song--author">{author}</div>
+      <div className="song--separator">&nbsp;—&nbsp;</div>
+      <div className="song--title">{title}</div>
     </div>
   );
 }
