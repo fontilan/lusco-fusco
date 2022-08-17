e2b44f290a8a151cdda615f45bb5e5a301ed9cf4
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-17 12:04:21 +0200

    change 4 divs to 1 p

--- src/Songs.js
+++ src/Songs.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 
 function Songs({ author, title }) {
   return (
-    <div className="song">
-      <div className="song--author">{author}</div>
-      <div className="song--separator">&nbsp;—&nbsp;</div>
-      <div className="song--title">{title}</div>
-    </div>
+    <p className="song">
+      <span className="song--author">{author}</span>&nbsp;—&nbsp;
+      <span className="song--title">{title}</span>
+    </p>
   );
 }
 
