25ed6521fab04fc83c6b3d26a003eec7c5533346
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-22 12:53:20 +0200

    add function to toggle dark/light theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
+import useLocalStorage from 'use-local-storage';
 import { nanoid } from 'nanoid';
-import musicData from './assets/songsDB';
+
 import Songs from './Songs';
 import cover from './assets/cover.webp';
+import musicData from './assets/songsDB';
 
 const altText = "Peter Ilsted's artwork titled En druser";
 
@@ -14,8 +17,24 @@ function App() {
     <Songs author={song.author} title={song.title} key={nanoid()} />
   ));
 
+  // check if the user prefers dark theme
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  // use dark theme if the user prefers it, otherwise use light theme
+  // (theme variable is null initially, so instead we use the second, default value. After it's set and saved with the switchTheme function it will be used after refresh / next visit)
+  const [theme, setTheme] = useLocalStorage(
+    'theme',
+    defaultDark ? 'dark' : 'light',
+  );
+
+  // switch the theme variable from light to dark and vice versa & save it in Local Storage
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
       <div>
         <h1 className="title">lusco-fusco</h1>
         <p className="sub-title">
@@ -25,6 +44,9 @@ function App() {
       <a className="link" href={spotifyLink} target="_blank" rel="noreferrer">
         Listen on Spotify
       </a>
+      <button type="button" onClick={switchTheme}>
+        Switch to {theme === 'dark' ? 'light' : 'dark'} theme
+      </button>
       <h2 className="date">{month}</h2>
       <div className="main">
         <div className="main__cover">
