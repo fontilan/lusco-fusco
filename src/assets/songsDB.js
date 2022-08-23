b4cde149091917411f86505219ed7fcc5c882286
fontilan <68748054+fontilan@users.noreply.github.com>
2022-08-23 10:51:34 +0200

    flatten array to a single object

--- src/assets/songsDB.js
+++ src/assets/songsDB.js
@@ -1,91 +1,88 @@
-const musicData = [
-  {
-    date: 'August 2022',
-    link: 'https://open.spotify.com/playlist/7Lz7wRpOszolsTFfFe6VmB?si=0c82ea7dcb844265',
+const musicData = {
+  date: 'August 2022',
+  link: 'https://open.spotify.com/playlist/7Lz7wRpOszolsTFfFe6VmB?si=0c82ea7dcb844265',
 
-    songs: [
-      {
-        author: 'Raffy Bushman',
-        title: 'Olympus',
-      },
-      {
-        author: 'Gwenno',
-        title: 'Anima',
-      },
-      {
-        author: 'jennylee',
-        title: 'Tickles',
-      },
-      {
-        author: 'Cosmic Analog Ensemble',
-        title: 'À bras le coeur',
-      },
-      {
-        author: 'Guided By Voices',
-        title: 'Echos Myron',
-      },
-      {
-        author: 'Boof',
-        title: 'Chicken With Waffles',
-      },
-      {
-        author: 'Kadhja Bonet',
-        title: 'Dear Gina',
-      },
-      {
-        author: 'Manfredo Fest',
-        title: 'Braziliana Nº1',
-      },
-      {
-        author: 'The Space Lady',
-        title: 'Major Tom',
-      },
-      {
-        author: 'Sun Ra, Sun Ra & His Arkestra',
-        title: "That's How I Feel - Remastered",
-      },
-      {
-        author: 'Earl Sweatshirt',
-        title: '2010',
-      },
-      {
-        author: 'Hala Strana',
-        title: 'Stouthrief',
-      },
-      {
-        author: 'Caterina Barbieri, Walter Zanetti',
-        title: 'Fantas for Electric Guitar',
-      },
-      {
-        author: 'Dionne Warwick',
-        title: "You're Gonna Need Me",
-      },
-      {
-        author: 'Laboratorium, Janusz Grzywacz',
-        title: 'Zdrowie na budowie',
-      },
-      {
-        author: 'Edan, Insight Innovates',
-        title: 'Funky Voltron',
-      },
-      {
-        author: 'Time Wharp',
-        title: 'Mixo World',
-      },
-      {
-        author: 'PVA',
-        title: 'Hero Man',
-      },
-      {
-        author: 'Radioactive Man',
-        title: 'Goodnight Morton',
-      },
-      {
-        author: 'Trees',
-        title: 'The Garden of Jane Delawney',
-      },
-    ],
-  },
-];
-
+  songs: [
+    {
+      author: 'Raffy Bushman',
+      title: 'Olympus',
+    },
+    {
+      author: 'Gwenno',
+      title: 'Anima',
+    },
+    {
+      author: 'jennylee',
+      title: 'Tickles',
+    },
+    {
+      author: 'Cosmic Analog Ensemble',
+      title: 'À bras le coeur',
+    },
+    {
+      author: 'Guided By Voices',
+      title: 'Echos Myron',
+    },
+    {
+      author: 'Boof',
+      title: 'Chicken With Waffles',
+    },
+    {
+      author: 'Kadhja Bonet',
+      title: 'Dear Gina',
+    },
+    {
+      author: 'Manfredo Fest',
+      title: 'Braziliana Nº1',
+    },
+    {
+      author: 'The Space Lady',
+      title: 'Major Tom',
+    },
+    {
+      author: 'Sun Ra, Sun Ra & His Arkestra',
+      title: "That's How I Feel - Remastered",
+    },
+    {
+      author: 'Earl Sweatshirt',
+      title: '2010',
+    },
+    {
+      author: 'Hala Strana',
+      title: 'Stouthrief',
+    },
+    {
+      author: 'Caterina Barbieri, Walter Zanetti',
+      title: 'Fantas for Electric Guitar',
+    },
+    {
+      author: 'Dionne Warwick',
+      title: "You're Gonna Need Me",
+    },
+    {
+      author: 'Laboratorium, Janusz Grzywacz',
+      title: 'Zdrowie na budowie',
+    },
+    {
+      author: 'Edan, Insight Innovates',
+      title: 'Funky Voltron',
+    },
+    {
+      author: 'Time Wharp',
+      title: 'Mixo World',
+    },
+    {
+      author: 'PVA',
+      title: 'Hero Man',
+    },
+    {
+      author: 'Radioactive Man',
+      title: 'Goodnight Morton',
+    },
+    {
+      author: 'Trees',
+      title: 'The Garden of Jane Delawney',
+    },
+  ],
+};
 export default musicData;
